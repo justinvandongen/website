dbb7709d71ad89772658a77594c8140f836cc1b9
lorenzo nannings <lorenzonannings@gmail.com>
2016-09-22 21:27:32 +0200

    google analatics, code cleanup, marges
    -google analatics tracking code toegevoegd aan start.js
    -start.js aan alle documenten toegevoegd
    -volgorde scripts verbeterd
    -marge logo in nav gelijk gemaakt met die van inloggen

--- js/start.js
+++ js/start.js
@@ -1,6 +1,14 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ ga('create', 'UA-84606648-1', 'auto');
+ ga('send', 'pageview');
+
 function werktNiet(){
-  alert("deze functie werkt niet in de demo");
+  alert("Deze functie werkt niet in de demo.");
 }
 function WIP(){
-  alert("hier word aan gewerkt \nwil je mee helpen? ga dan naar onze github pagina");
+  alert("Hier word nog aan gewerkt \nWil je mee helpen? ga dan naar onze github pagina.");
 }
